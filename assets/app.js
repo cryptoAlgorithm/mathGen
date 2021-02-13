30302a28a3c46e5c288106abff16967d24feea57
CryptoAlgo Inc <support@cryptoalgo.cf>
2021-02-13 13:45:12 +0800

    Refactored CSS and added help page

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,28 @@
+// *----------------------------* //
+// ====== Really Important ====== //
+// ############################## //
+
+// Returns the document with the specified ID
+// (shortens the getElementById function)
 const $ = (e) => { return document.getElementById(e) }
+
+// ----------------------- //
+// ====== Constants ====== //
+// ----------------------- //
+
+// Application version
+const VERSION_CODE = "0.1.5";
+
+// Input fields
+const numGen = $('num-gen').MDCTextField;
+const minField = $('ans-min').MDCTextField;
+const maxField = $('ans-max').MDCTextField;
+
+
+// ------------------------------- //
+// ====== Utility Functions ====== //
+// ------------------------------- //
+
 
 // Select contents of elem for copying
 function copyElemContents(el) {
@@ -26,23 +50,100 @@ function copyElemContents(el) {
     el.classList.remove('copy');
 }
 
-const checkboxClickListener = (e) => {
-    console.log('event')
-    $('grp-mdc').MDCCheckbox.disabled = true;
-    $('per-sq-mdc').MDCCheckbox.disabled = true;
-    $('tri-mdc').MDCCheckbox.disabled = true;
-    $('general-form-mdc').MDCCheckbox.disabled = true;
-    if (e.target.checked) {
-        e.target.disabled = false;
-    }
-}
-
+// Show a snackbar with a message
 const showMSG = (msg) => {
-    const snackbar = $('msg-snackbar')  .MDCSnackbar;
+    const snackbar = $('msg-snackbar').MDCSnackbar;
     snackbar.labelText = msg;
     snackbar.open();
 }
 
+// Creates a table with questions and add it to the target elem
+const constructQnTable = (targetElem, qns, options, print = false) => {
+    let tableContent = '';
+
+    if (print) tableContent += '<div class="printMsg">Reloading this window will cause generated questions to be lost</div>'
+
+    if (options.noFormat) tableContent = `<pre>${qns.map((qn, i) => {
+        if (options.qnNos) return `${i}. ${qn}`;
+        else return qn;
+    }).join('\n')}</pre>`
+    else {
+        // Header
+        if (options.header != null) tableContent += `<h2><b>${options.header}</b></h2>`;
+
+        // Name field
+        if (options.name) tableContent +=
+            '<p style="margin:0">Name: <u>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</u></p>';
+        // Class field
+        if (options.class) tableContent +=
+            '<p style="margin:8px 0 0 0">Class: <u>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</u></p>';
+        // Date field
+        if (options.date) tableContent +=
+            '<p style="margin-top:8px">Date: <u>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</u></p>';
+
+        tableContent += '<table>';
+        let i = 1;
+        qns.forEach((eqn) => {
+            if ((i - 1) % options.rows === 0 && i !== 0) tableContent += '</tr><tr>';
+            if (options.qnNos) tableContent += `<td>${i}. </td>`;
+            else tableContent += '<td></td>'; // I don't know why an extra td is required else the layout gets wonky
+            tableContent += `<td class="qn">${eqn}</td>`;
+            i++;
+        });
+        tableContent += '</tr><td></td>';
+
+        // Footer
+        // tableContent += '<h2><b>Explore, explode, expel (test footer)</b></h2>';
+    }
+
+    // Add class to parent
+    targetElem.classList.add('qnOutput');
+
+    // Add html content to the target element
+    targetElem.innerHTML = tableContent;
+}
+
+
+// ----------------------------------- //
+// ====== User Input Validation ====== //
+// ----------------------------------- //
+
+function inputValid() {
+    // Check validity of min-max values
+    const minRange = minField.value;
+    const maxRange = maxField.value;
+    if (maxRange === minRange || !maxField.valid || !minField.valid) return false;
+
+    // Validate number of qns to generate
+    if (!numGen.valid) return false;
+
+    // If the execution reached this point, everything is valid
+    return true;
+}
+
+const genBtn = $('start_gen');
+function inputChangeListener() {
+    genBtn.disabled = !inputValid();
+    console.log('called');
+}
+
+
+// ----------------------- //
+// ====== Init Code ====== //
+// ----------------------- //
+
+// Display app version
+$('app-ver').textContent = `App version: v${VERSION_CODE}`;
+
+// Validate input for the first time
+inputChangeListener();
+
+
+// ------------------------------------- //
+// ====== Various event listeners ====== //
+// ------------------------------------- //
+
+// Reset factorisation options
 const resetFactOps = () => {
     $('c-factor').checked = false;
     $('grp').checked = false;
@@ -51,11 +152,22 @@ const resetFactOps = () => {
     $('general-form').checked = false;
 }
 
+// .......................................... //
+// ------ Input field change listeners ------ //
+// .......................................... //
+
+numGen.input_.oninput = inputChangeListener;
+maxField.input_.oninput = inputChangeListener;
+minField.input_.oninput = inputChangeListener;
+
+// ............................... //
+// ------ Onclick listeners ------ //
+// ............................... //
+
+// Reset factorisation methods button onclick listener
 $('reset-methods').onclick = resetFactOps;
 
-const VERSION_CODE = "0.1.5";
-
-// Set up onclick listeners
+// More info FAB onclick listener
 $('more-info').onclick = () => {
     const dialog = $('more-info-dialog').MDCDialog;
     dialog.open();
@@ -67,105 +179,48 @@ $('more-info').onclick = () => {
             case 'settings':
                 $('settings-dialog').MDCDialog.open();
                 break;
+            case 'help':
+                window.open('help.html', 'Help',
+                    'toolbars=0,width=580,height=720,left=200,top=200,scrollbars=1,resizable=1,modal=1,alwaysRaised=1');
+                break;
         }
     })
 }
 
-// Display app version
-$('app-ver').textContent = `App version: v${VERSION_CODE}`;
-
-// ------
-// Creates a table with questions and add it to the target elem
-const constructQnTable = (targetElem, qns, options, print = false) => {
-    let tableContent = '';
-
-    if (print) {
-        tableContent += '<div class="printMsg">Reloading this window will cause generated questions to be lost</div>'
-    }
-
-    // Header
-    if (options.header != null) tableContent += `<h2><b>${options.header}</b></h2>`;
-
-    // Add name field
-    if (options.name) tableContent +=
-        '<p style="margin:0">Name: <span style="letter-spacing:0px">＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿</span></p>';
-    // Class field
-    if (options.class) tableContent +=
-        '<p style="display:inline-block;margin-top:2px">Class: <span style="letter-spacing:0px">＿＿＿＿＿＿＿＿＿＿</span></p>';
-    // Date field
-    if (options.date) tableContent +=
-        '<p style="float:right;margin-top:2px">Date: <span style="letter-spacing:0px">＿＿＿＿＿＿＿＿＿＿</span></p>';
-
-    tableContent += '<table>';
-    let i = 1;
-    qns.forEach((eqn) => {
-        if ((i - 1) % options.rows === 0 && i !== 0) tableContent += '</tr><tr>';
-        tableContent += `<td>${i}. </td><td class="qn">${eqn}</td>`;
-        i++;
-    });
-    tableContent += '</tr><td></td>';
-
-    // Footer
-    // tableContent += '<h2><b>Explore, explode, expel (test footer)</b></h2>';
-
-    // Add class to parent
-    targetElem.classList.add('qnOutput');
-
-    // Add html content to the target element
-    targetElem.innerHTML = tableContent;
-}
-
-// Generate button onclick
-$('start_gen').onclick = () => {
+// Generate button onclick listener
+genBtn.onclick = () => {
     // Set config vars based on user input
-    // Check validity of input
-    const minTextField = $('ans-min').MDCTextField;
-    const maxTextField = $('ans-max').MDCTextField;
-    const minRange = minTextField.value;
-    const maxRange = maxTextField.value;
-    if (Math.abs(maxRange - minRange) < 2 || !maxTextField.valid || !minTextField.valid) {
-        showMSG('Invalid minimum or maximum range');
-        return;
-    }
+    MIN_RANGE = parseInt(minField.value);
+    MAX_RANGE = parseInt(maxField.value);
 
-    MIN_RANGE = minRange;
-    MAX_RANGE = maxRange;
+    const n = numGen.value; // Number of questions to generate
 
-    // Validate number of qns to generate
-    const numQnsField = $('num-gen').MDCTextField;
-    if (!numQnsField.valid) {
-        showMSG('Invalid number of questions to generate');
-        return;
-    }
-
-    const numGen = numQnsField.value;
-    const start = new Date();
     let results = [];
     if ($('grp').checked) {
-        for (let i = 0; i < numGen; i++) {
+        for (let i = 0; i < n; i++) {
             const {a, b, x, y, use_vars} = _grouping();
             results.push(grouping(a, b, x, y, use_vars).qn);
         }
     }
     else if ($('per-sq').checked) {
-        for (let i = 0; i < numGen; i++) {
+        for (let i = 0; i < n; i++) {
             const {a, b, c, use_vars, two_var} = _perfect_square();
             results.push(perfect_square(a, b, c, use_vars, two_var).qn);
         }
     }
     else if ($('tri').checked) {
-        for (let i = 0; i < numGen; i++) {
+        for (let i = 0; i < n; i++) {
             const {a, b, c, use_vars, x1, x2} = _trinomial();
             results.push(trinomial(a, b, c, use_vars, x1, x2).qn);
         }
     }
     else if ($('c-factor').checked) {
-        for (let i = 0; i < numGen; i++) {
+        for (let i = 0; i < n; i++) {
             results.push(common_factor().qn);
         }
     }
     else if ($('general-form').checked) {
-        for (let i = 0; i < numGen; i++) {
+        for (let i = 0; i < n; i++) {
             const {a, b, c, use_vars, x1, x2} = _general_formula();
             results.push(general_formula(a, b, c, use_vars, x1, x2).qn);
         }
@@ -175,13 +230,20 @@ $('start_gen').onclick = () => {
         return;
     }
 
+    const selectedChips = $('out-items-chips').MDCChipSet.selectedChipIds; // Selected chips
+
+    console.log({MIN_RANGE, MAX_RANGE});
+
     const options = {
         rows: 2,
-        name: true,
-        date: true,
-        class: true,
-        header: 'This is a test'
+        name: selectedChips.includes('out-name-field'),
+        date: selectedChips.includes('out-date-field'),
+        class: selectedChips.includes('out-class-field'),
+        qnNos: selectedChips.includes('out-qn-nos'),
+        header: 'This is a test',
+        noFormat: $('no-format').checked
     }
+
     constructQnTable($('results-content'), results, options);
 
     // Open the dialog
@@ -197,7 +259,7 @@ $('start_gen').onclick = () => {
                     constructQnTable(win.document.body, results, options, true);
 
                     // Set afterprint event
-                    window.onafterprint = () => {
+                    win.onafterprint = () => {
                         // Close window after a delay
                         setTimeout(() => win.close(), 500);
                     };
@@ -207,9 +269,6 @@ $('start_gen').onclick = () => {
                 break;
         }
     });
-
-    console.log('Time:', new Date() - start);
-    // console.log('Done')
 }
 
 // Copy question onclick listener
--- assets/lib.js
+++ assets/lib.js
@@ -23,15 +23,13 @@ function sample(arr, n) {
     return result;
 }
 
-const isSquare = function (n) {
-    return n > 0 && Math.sqrt(n) % 1 === 0;
-};
-
 function randrange(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
+const randint = (min, realMax) => { return randrange(min, realMax + 1); }
 
 function gcd(x, y) {
     if ((typeof x !== 'number') || (typeof y !== 'number'))
@@ -90,15 +88,18 @@ function _general_formula() {
     let b = 0;
     let c = 0;
 
+    let x1 = 0;
+    let x2 = 0;
+
     while (b ** 2 - 4 * a * c < 0 ||
     b === 0 ||
     c === 0 ||
-    gcd(a, gcd(b, c)) !== 1 || isInt(x1) || isInt(x)) {
-        a = randrange(MIN_RANGE, MAX_RANGE)
-        b = randrange(MIN_RANGE, MAX_RANGE)
-        c = randrange(MIN_RANGE, MAX_RANGE)
-        let x1 = (-b + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
-        let x2 = (-b - Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
+    gcd(a, gcd(b, c)) !== 1 || !isInt(x1) || !isInt(x2)) {
+        a = randint(MIN_RANGE, MAX_RANGE)
+        b = randint(MIN_RANGE, MAX_RANGE)
+        c = randint(MIN_RANGE, MAX_RANGE)
+        x1 = (-b + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
+        x2 = (-b - Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
     }
 
     while (!isInt(x1) || !isInt(x2) ||
@@ -106,17 +107,15 @@ function _general_formula() {
     b === 0 ||
     c === 0 ||
     gcd(a, gcd(b, c)) !== 1) {
-        a = randrange(MIN_RANGE, MAX_RANGE)
-        b = randrange(MIN_RANGE, MAX_RANGE)
-        c = randrange(MIN_RANGE, MAX_RANGE)
+        a = randint(MIN_RANGE, MAX_RANGE)
+        b = randint(MIN_RANGE, MAX_RANGE)
+        c = randint(MIN_RANGE, MAX_RANGE)
         x1 = (-b + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
         x2 = (-b - Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
     }
     let use_vars = vars[randrange(0, vars.length)];
 
     return {a, b, c, use_vars, x1, x2};
-
-
 }
 
 
@@ -140,13 +139,13 @@ function general_formula(a, b, c, use_vars, x1, x2) {
 // common_factor.py
 
 function common_factor() {
-    const length = randrange(LENGTH_MIN_RANGE, LENGTH_MAX_RANGE);
-    const factor = randrange(COMMON_MIN_RANGE, COMMON_MAX_RANGE);
+    const length = randint(LENGTH_MIN_RANGE, LENGTH_MAX_RANGE);
+    const factor = randint(COMMON_MIN_RANGE, COMMON_MAX_RANGE);
     const use_vars = sample(vars, length);
     let qn = '';
     for (let i = 0; i < length; i++) {
-        let r = randrange(MIN_RANGE, MAX_RANGE);
-        while (r === 0) r = randrange(MIN_RANGE, MAX_RANGE);
+        let r = randint(MIN_RANGE, MAX_RANGE);
+        while (r === 0) r = randint(MIN_RANGE, MAX_RANGE);
         if (i === 0) qn += format_coeff1(r * factor) + use_vars[i];
         else qn += format_coeff2(r *factor) + use_vars[i];
     }
@@ -164,14 +163,14 @@ function _trinomial() {
     let a = 0, b = 0, c = 0, x1 = 0, x2 = 0, f1 = 0, f2 = 0;
 
     while (gcd(a, gcd(b, c)) != 1 || b == 0) {
-        f1 = randrange(MIN_RANGE, MAX_RANGE)
-        f2 = randrange(MIN_RANGE, MAX_RANGE)
-        x1 = randrange(MIN_RANGE, MAX_RANGE)
-        x2 = randrange(MIN_RANGE, MAX_RANGE)
-        while (f1 === 0) f1 = randrange(MIN_RANGE, MAX_RANGE)
-        while (f2 === 0) f2 = randrange(MIN_RANGE, MAX_RANGE)
-        while (x1 === 0) x1 = randrange(MIN_RANGE, MAX_RANGE)
-        while (x2 === 0) x2 = randrange(MIN_RANGE, MAX_RANGE)
+        f1 = randint(MIN_RANGE, MAX_RANGE)
+        f2 = randint(MIN_RANGE, MAX_RANGE)
+        x1 = randint(MIN_RANGE, MAX_RANGE)
+        x2 = randint(MIN_RANGE, MAX_RANGE)
+        while (f1 === 0) f1 = randint(MIN_RANGE, MAX_RANGE)
+        while (f2 === 0) f2 = randint(MIN_RANGE, MAX_RANGE)
+        while (x1 === 0) x1 = randint(MIN_RANGE, MAX_RANGE)
+        while (x2 === 0) x2 = randint(MIN_RANGE, MAX_RANGE)
         a = x1 * x2;
         b = f1 * x2 + f2 * x1;
         c = f1 * f2;
@@ -206,15 +205,15 @@ function _grouping() {
     // generate an expression to be factorised in the form (x-a)(y-b)
     let a = 0; let b = 0; let x = 0; let y = 0;
     while (gcd(a * x, gcd(a * y, gcd(b * x, b * y))) !== 1) {
-        a = randrange(MIN_RANGE, MAX_RANGE);
-        b = randrange(MIN_RANGE, MAX_RANGE);
-        x = randrange(MIN_RANGE, MAX_RANGE);
-        y = randrange(MIN_RANGE, MAX_RANGE);
+        a = randint(MIN_RANGE, MAX_RANGE);
+        b = randint(MIN_RANGE, MAX_RANGE);
+        x = randint(MIN_RANGE, MAX_RANGE);
+        y = randint(MIN_RANGE, MAX_RANGE);
 
-        while (a === 0) a = randrange(MIN_RANGE, MAX_RANGE);
-        while (b === 0) b = randrange(MIN_RANGE, MAX_RANGE);
-        while (x === 0) x = randrange(MIN_RANGE, MAX_RANGE);
-        while (y === 0) y = randrange(MIN_RANGE, MAX_RANGE);
+        while (a === 0) a = randint(MIN_RANGE, MAX_RANGE);
+        while (b === 0) b = randint(MIN_RANGE, MAX_RANGE);
+        while (x === 0) x = randint(MIN_RANGE, MAX_RANGE);
+        while (y === 0) y = randint(MIN_RANGE, MAX_RANGE);
     }
 
 
@@ -243,21 +242,21 @@ function grouping(a, b, x, y, use_vars) {
 
 function common_grouping() {
     const {a, b, x, y, use_vars} = _grouping();
-    const factor = randrange(COMMON_MIN_RANGE, COMMON_MAX_RANGE);
+    const factor = randint(COMMON_MIN_RANGE, COMMON_MAX_RANGE);
     return grouping(a * factor, b * factor, x * factor, y * factor, use_vars);
 }
 
 
 function common_general_formula() {
     const {a, b, c, use_vars, x1, x2} = _general_formula();
-    const factor = randrange(COMMON_MIN_RANGE, COMMON_MAX_RANGE);
+    const factor = randint(COMMON_MIN_RANGE, COMMON_MAX_RANGE);
     return general_formula(a * factor, b * factor, c * factor, use_vars, x1, x2);
 }
 
 
 function common_trinomial() {
     const {a, b, c, use_vars, x1, x2} = _trinomial();
-    const factor = randrange(COMMON_MIN_RANGE, COMMON_MAX_RANGE);
+    const factor = randint(COMMON_MIN_RANGE, COMMON_MAX_RANGE);
     return trinomial(a * factor, b * factor, c * factor, use_vars, x1, x2);
 }
 
@@ -270,8 +269,8 @@ function common_trinomial() {
 function _perfect_square() {
     let a = 0; let b = 0; let c = 0;
     while (a === 0 || b === 0 || c === 0 || gcd(a, gcd(b, c)) !== 1) {
-        const x = randrange(MIN_RANGE, MAX_RANGE);
-        const y = randrange(MIN_RANGE, MAX_RANGE);
+        const x = randint(MIN_RANGE, MAX_RANGE);
+        const y = randint(MIN_RANGE, MAX_RANGE);
         a = x ** 2;
         b = 2 * x * y;
         c = y ** 2;
@@ -279,7 +278,7 @@ function _perfect_square() {
 
     let two_var = false;
     let use_vars;
-    if (randrange(2) === 1) {
+    if (randrange(0, 2) === 1) {
         use_vars = sample(vars, 2);
         two_var = true;
     }
